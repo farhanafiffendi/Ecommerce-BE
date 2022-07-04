c7ed22811e6d8c18e659231cace626f028a9840e
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-07-04 17:45:12 +0700

    first commit

---
+++ app/Models/Customer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'email', 'email_verified_at', 'password', 'remember_token'
+    ];
+
+    /**
+     * invoice
+     *
+     * @return void
+     */
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+
+    /**
+     * reviews
+     *
+     * @return void
+     */
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
